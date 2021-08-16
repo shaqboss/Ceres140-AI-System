c445046af5da3016b4cd2fcbd3149467d2e0f97d
Python Master <83932005+shaqboss@users.noreply.github.com>
2021-08-16 22:37:00 +0000

    Bot 1.2

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import os
 
 name = input("UserName:")
-bot = commands.Bot(command_prefix='$')
+bot = commands.Bot(command_prefix='>')
 
 
 @bot.event
@@ -13,11 +13,24 @@ async def on_connect():
 
 @bot.command()
 async def Start(ctx):
-    await ctx.send("Hello am Ceres140 Ai system")
+    await ctx.send("🤖 Hello am Ceres140 Ai system")
+
 
 @bot.command()
 async def Ceres140(ctx):
-	await ctx.send("What Maye I do For You Master")
+    await ctx.send("What Maye I do For You Master 👑")
+
+
+@bot.command()
+async def Ceres140_How_Are_You(ctx):
+    await ctx.send("Am Good and Running I hope You are")
+
+
+@bot.command()
+async def Ceres140_Greet(ctx):
+    await ctx.send("Hello Welcome Too Our Group Am Your Friendly Ai System Max"
+                   )
+
 
 my_secret = os.environ['Token']
 bot.run(my_secret)
